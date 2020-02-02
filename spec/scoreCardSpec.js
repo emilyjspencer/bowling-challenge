5c1f43c95c45d4da7f92ad12194376cc50497963
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-02 12:50:57 +0000

    Player can roll

--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -8,10 +8,9 @@ describe('ScoreCard', function(){
     scoreCard = new ScoreCard();
   })
 
-  
     describe('frame zero', function(){
       it('the current score is zero before any rolls have been made', function(){
-        expect(scoreCard.getCurrentScore()).toEqual(0) 
+        expect(scoreCard.getCurrentScoreForFrame()).toEqual(0) 
       })
 
       it('the frames array is empty at the start of the game', function(){
@@ -23,8 +22,24 @@ describe('ScoreCard', function(){
       })
 
       it('the score so far is zero before any rolls have been made', function() {
-        expect(scoreCard.getScoreSoFar()).toEqual(0)
+        expect(scoreCard.getCumulativeScore()).toEqual(0)
       })
+    })
+
+    describe('playing game', function() {
+      it('gutter game  - the current score is 0 if the player scores 0 in roll1 and 0 in roll2 of frame1', function(){
+        expect(scoreCard.roll(0,0)).toEqual(0)
+        //expect(scoreCard.showMessage()).toEqual('Gutter Game Alert!!!')
+      })
+
+      it('normal game - the current score for frame is 7 if the player scores 2 in roll1 and 5 in roll2', function(){
+        expect(scoreCard.roll(2,5)).toEqual(7)
+      })
+
+      it('spare - the current score is 10 if the player scores 5 in roll1 and 5 in roll2', function() {
+        expect(scoreCard.roll(5,5)).toEqual(10)
+      })
+
     })
 
   })
--- src/scoreCard.js
+++ src/scoreCard.js
@@ -5,18 +5,19 @@ function ScoreCard() {
     //this.inPlay = false; // is the game in play - true or false - upon initialization - not in game - not playing
     ////this.frameRollCount = []; // tracks whether the player is on their first or second roll of a frame 
     //this.scoreSoFar = []; // score so far
-    //this.isStrike = false // returns true or false but on initialization is false
-    //this.isSpare = false // returns true of false but on initialization - is false
+     this.isStrike = false // returns true or false but on initialization is false
+     this.isSpare = false // returns true of false but on initialization - is false
     //this.frameNumber = 0 // 0 as no rolls have been made
     //this.frameRollNumber = 0 // - as player hasn't started frame yet
+      this.isGutterGame = false
       this.frames = [];
       this.frameCount = 0
-      this.currentScore = 0;
-      this.scoreSoFar = 0;
+      this.currentScoreForFrame = 0;
+      this.cumulativeScore = 0;
     }
     
-    ScoreCard.prototype.getCurrentScore = function() {
-      return this.currentScore;
+    ScoreCard.prototype.getCurrentScoreForFrame = function() {
+      return this.currentScoreForFrame;
     }
 
     ScoreCard.prototype.getFrames = function() {
@@ -27,7 +28,14 @@ function ScoreCard() {
       return this.frameCount;
     }
 
-    ScoreCard.prototype.getScoreSoFar = function() {
-      return this.scoreSoFar;
-    }    
+    ScoreCard.prototype.getCumulativeScore = function() {
+      return this.cumulativeScore;
+    }
+    
+    ScoreCard.prototype.roll = function(roll1, roll2) {
+       rolls =  roll1 + roll2
+       return rolls
+    }
+
+   
    
